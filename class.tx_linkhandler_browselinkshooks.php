a00e1c45e13ebe8e6baca277f1a3a85fd8b2088b
michael.klapper <michael.klapper@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-08 10:57:57 +0000

    Add some PHPDoc
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/linkhandler/trunk@18815 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_linkhandler_browselinkshooks.php
+++ class.tx_linkhandler_browselinkshooks.php
@@ -1,30 +1,29 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2007 Daniel P�tzinger 
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*  A copy is found in the textfile GPL.txt and important notices to the license
-*  from the author is found in LICENSE.txt distributed with these scripts.
-*
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  Copyright (c) 2008, Daniel P�tzinger <daniel.poetzinger@aoemedia.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
+if (!defined ('TYPO3_MODE'))
+	die ('Access denied.');
 
 /**
  * hook to adjust linkwizard (linkbrowser)
@@ -33,13 +32,12 @@
  * @package TYPO3
  * @subpackage linkhandler
  */
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
 
 // include defined interface for hook
 // (for TYPO3 4.x usage this interface is part of the patch)
 $confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['linkhandler']);
-if ($confArr['applyPatch']==1) {	
+if ($confArr['applyPatch']==1) {
 	require_once (t3lib_extMgm::extPath('linkhandler').'patch/interfaces/interface.t3lib_browselinkshook.php');
 }
 else {
@@ -55,14 +53,14 @@ require_once (t3lib_extMgm::extPath('linkhandler').'classes/class.tx_linkhandler
 
 
 class tx_linkhandler_browselinkshooks implements t3lib_browseLinksHook {
-	
+
 	/**
-	* the browse_links object 
+	* the browse_links object
 	*/
 	protected $pObj;
-	
+
 	protected $allAvailableTabHandlers=array();
-	
+
 	/**
 	 * initializes the hook object
 	 *
@@ -70,8 +68,8 @@ class tx_linkhandler_browselinkshooks implements t3lib_browseLinksHook {
 	 * @return	void
 	 */
 	function init($pObj,$params) {
-		$this->pObj=&$pObj;		
-		$this->_checkConfigAndGetDefault();	
+		$this->pObj=&$pObj;
+		$this->_checkConfigAndGetDefault();
 		$tabs=$this->getTabsConfig();
 		foreach ($tabs as $key=>$tabConfig) {
 			if ($this->isRTE()) {
@@ -79,15 +77,15 @@ class tx_linkhandler_browselinkshooks implements t3lib_browseLinksHook {
 			}
 		}
 		$this->allAvailableTabHandlers=$this->getAllRegisteredTabHandlerClassnames();
-		
+
 	}
-	
-	/* checks if 
+
+	/* checks if
 	*	$this->pObj->thisConfig['tx_linkhandler.'] is set, and if not it trys to load default from
 	*	TSConfig key mod.tx_linkhandler.
 	*	(in case the hook is called from a RTE, this configuration might exist because it is configured in RTE.defaul.tx_linkhandler)
-	*		In mode RTE: the parameter RTEtsConfigParams have to exist 
-	*		In mode WIzard: the parameter P[pid] have to exist 
+	*		In mode RTE: the parameter RTEtsConfigParams have to exist
+	*		In mode WIzard: the parameter P[pid] have to exist
 	*/
 	function _checkConfigAndGetDefault() {
 		global $BE_USER;
@@ -96,35 +94,35 @@ class tx_linkhandler_browselinkshooks implements t3lib_browseLinksHook {
 			$RTEsetup = $BE_USER->getTSConfig('RTE',t3lib_BEfunc::getPagesTSconfig($RTEtsConfigParts[5]));
 			$this->pObj->thisConfig = t3lib_BEfunc::RTEsetup($RTEsetup['properties'],$RTEtsConfigParts[0],$RTEtsConfigParts[2],$RTEtsConfigParts[4]);
 		}
-		
+
 		elseif (!is_array($this->pObj->thisConfig['tx_linkhandler.'])) {
 			$P=t3lib_div::_GP('P');
-			$pid=$P['pid'];			
+			$pid=$P['pid'];
 			$modTSconfig = $GLOBALS["BE_USER"]->getTSConfig("mod.tx_linkhandler",t3lib_BEfunc::getPagesTSconfig($pid));
 			//print_r($modTSconfig);
 			$this->pObj->thisConfig['tx_linkhandler.']=$modTSconfig['properties'];
 		}
-		
+
 	}
-	
+
 	/**
 	 * adds new items to the currently allowed ones and returns them
 	 *
 	 * @param	array	currently allowed items
 	 * @return	array	currently allowed items plus added items
 	 */
-	function addAllowedItems($allowedItems) {		
+	function addAllowedItems($allowedItems) {
 		if (is_array($this->pObj->thisConfig['tx_linkhandler.'])) {
-			foreach ($this->pObj->thisConfig['tx_linkhandler.'] as $name => $tabConfig) {				
+			foreach ($this->pObj->thisConfig['tx_linkhandler.'] as $name => $tabConfig) {
 				if (is_array($tabConfig)) {
 					$key=substr($name,0,-1);
 					$allowedItems[]=$key;
 				}
 			}
-		}		
+		}
 		return $allowedItems;
 	}
-	
+
 	/**
 	 * checks the current URL and returns a info array. This is used to
 	 *	tell the link browser which is the current tab based on the current URL.
@@ -136,56 +134,56 @@ class tx_linkhandler_browselinkshooks implements t3lib_browseLinksHook {
 	 * @return	array 				$info		a infoarray for browser to tell them what is current active tab
 	 */
 	function parseCurrentUrl($href,$siteUrl,$info) {
-		
-			//depending on link and setup the href string can contain complete absolute link			
+
+			//depending on link and setup the href string can contain complete absolute link
 			if (substr($href,0,7)=='http://') {
 				if ($_href=strstr($href,'?id=')) {
 					$href=substr($_href,4);
 				}
-				else {				
+				else {
 					$href=substr (strrchr ($href, "/"),1);
 				}
 			}
 			//ask the registered tabHandlers:
-			foreach ($this->allAvailableTabHandlers as $handler) {				
-				$result=call_user_func($handler.'::getLinkBrowserInfoArray',$href,$this->getTabsConfig());				
+			foreach ($this->allAvailableTabHandlers as $handler) {
+				$result=call_user_func($handler.'::getLinkBrowserInfoArray',$href,$this->getTabsConfig());
 				if (count($result)>0 && is_array($result)) {
-					
+
 					return array_merge($info,$result);
 				}
-			}							
+			}
 			return $info;
 	}
-	
+
 	/**
 	* returns a array of names available tx_linkhandler_tabHandler
 	*/
 	protected function getAllRegisteredTabHandlerClassnames() {
 		$default=array('tx_linkhandler_recordTab');
-		
+
 		if (is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['linkhandler/class.tx_linkhandler_browselinkshooks.php'])) {
-			foreach ($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['linkhandler/class.tx_linkhandler_browselinkshooks.php'] as $tabHandler) {	
+			foreach ($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['linkhandler/class.tx_linkhandler_browselinkshooks.php'] as $tabHandler) {
 				list($file,$class) = t3lib_div::revExplode(':',$tabHandler,2);
 				include_once($file);
-				$default[]=$class;		
+				$default[]=$class;
 			}
-		}	
+		}
 		return $default;
 	}
-	
+
 	/**
 	* returns the complete configuration (tsconfig) of all tabs
 	**/
-	private  function getTabsConfig() {		
+	private  function getTabsConfig() {
 		$tabs=array();
 		if (is_array($this->pObj->thisConfig['tx_linkhandler.'])) {
-			foreach ($this->pObj->thisConfig['tx_linkhandler.'] as $name => $tabConfig) {	
+			foreach ($this->pObj->thisConfig['tx_linkhandler.'] as $name => $tabConfig) {
 				if (is_array($tabConfig)) {
 					$key=substr($name,0,-1);
 					$tabs[$key]=$tabConfig;
-				}				
+				}
 			}
-		}		
+		}
 		return $tabs;
 	}
 	/**
@@ -195,7 +193,7 @@ class tx_linkhandler_browselinkshooks implements t3lib_browseLinksHook {
 		$conf=$this->getTabsConfig();
 		return $conf[$tabKey];
 	}
-	
+
 	/**
 	 * modifies the menu definition and returns it
 	 *
@@ -209,12 +207,12 @@ class tx_linkhandler_browselinkshooks implements t3lib_browseLinksHook {
 			$menuDef[$key]['label'] = $tabConfig['label']; // $LANG->getLL('records',1);
 			$menuDef[$key]['url'] = '#';
 			$addPassOnParams.=$this->getaddPassOnParams();
-			$menuDef[$key]['addParams'] = 'onclick="jumpToUrl(\'?act='.$key.'&editorNo='.$this->pObj->editorNo.'&contentTypo3Language='.$this->pObj->contentTypo3Language.'&contentTypo3Charset='.$this->pObj->contentTypo3Charset.$addPassOnParams.'\');return false;"';								
+			$menuDef[$key]['addParams'] = 'onclick="jumpToUrl(\'?act='.$key.'&editorNo='.$this->pObj->editorNo.'&contentTypo3Language='.$this->pObj->contentTypo3Language.'&contentTypo3Charset='.$this->pObj->contentTypo3Charset.$addPassOnParams.'\');return false;"';
 		}
-		
+
 		return $menuDef;
 	}
-//MKL	
+//MKL
 	/**
 	* returns additional addonparamaters - required to keep several informations for the RTE linkwizard
 	**/
@@ -223,16 +221,16 @@ class tx_linkhandler_browselinkshooks implements t3lib_browseLinksHook {
 		if (!$this->isRTE()) {
 			$P2=t3lib_div::_GP('P');
 			if (is_array($P2) && !empty($P2) ) {
-			
+
 				$urlParams = t3lib_div::implodeArrayForUrl('P',$P2);
-			}	
+			}
 		}
 		return $urlParams;
 	}
-	
+
 	/**
 	* returns if the current linkwizard is RTE or not
-	**/ 
+	**/
 	protected function isRTE() {
 		if ($this->pObj->mode=='rte') {
 			return true;
@@ -240,9 +238,9 @@ class tx_linkhandler_browselinkshooks implements t3lib_browseLinksHook {
 		else {
 			return false;
 		}
-		
+
 	}
-	
+
 	/**
 	 * returns a new tab for the browse links wizard
 	 *
@@ -250,17 +248,17 @@ class tx_linkhandler_browselinkshooks implements t3lib_browseLinksHook {
 	 * @return	string		a tab for the selected link action
 	 */
 	function getTab($act) {
-		
+
 		global $LANG;
 		if (!$this->_isOneOfLinkhandlerTabs($act))
 		    return false;
-		    
-		if ($this->isRTE()) {			
+
+		if ($this->isRTE()) {
 			if (isset($this->pObj->classesAnchorJSOptions)) {
-				$this->pObj->classesAnchorJSOptions[$act]=@$this->pObj->classesAnchorJSOptions['page']; //works for 4.1.x patch, in 4.2 they make this property protected! -> to enable classselector in 4.2 easoiest is to path rte. 
+				$this->pObj->classesAnchorJSOptions[$act]=@$this->pObj->classesAnchorJSOptions['page']; //works for 4.1.x patch, in 4.2 they make this property protected! -> to enable classselector in 4.2 easoiest is to path rte.
 			}
 		}
-		
+
 		$configuration=$this->getTabConfig($act);
 		//get current href value (diffrent for RTE and normal browselinks)
 		if ($this->isRTE()) {
@@ -275,11 +273,11 @@ class tx_linkhandler_browselinkshooks implements t3lib_browseLinksHook {
 			$tabHandlerClass=$configuration['tabHandler'];
 		}
 		$tabHandler=new $tabHandlerClass($this->pObj,$this->getaddPassOnParams,$configuration,$currentValue,$this->isRTE());
-		$content=$tabHandler->getTabContent();	
-			
+		$content=$tabHandler->getTabContent();
+
 		return $content;
 	}
-	
+
 
     function _isOneOfLinkhandlerTabs ($key)
     {
