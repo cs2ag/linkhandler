82d49c5482cabec5425de873a74e4619d570111d
michael.klapper <michael.klapper@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-04-08 09:46:59 +0000

    Add some PHPDoc
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/linkhandler/trunk@18811 735d13b6-9817-0410-8766-e36946ffe9aa

--- classes/class.tx_linkhandler_recordTab.php
+++ classes/class.tx_linkhandler_recordTab.php
@@ -1,47 +1,44 @@
 <?php
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2007 Daniel P�tzinger
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*  A copy is found in the textfile GPL.txt and important notices to the license
-*  from the author is found in LICENSE.txt distributed with these scripts.
-*
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  Copyright (c) 2008, Daniel P�tzinger <daniel.poetzinger@aoemedia.de>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
+if (!defined ('TYPO3_MODE'))
+	die ('Access denied.');
+
+require_once t3lib_extMgm::extPath('linkhandler') . 'classes/record/class.tx_linkhandler_recordsTree.php';
+require_once t3lib_extMgm::extPath('linkhandler') . 'classes/record/class.TBE_browser_recordListRTE.php';
+require_once t3lib_extMgm::extPath('linkhandler') . 'classes/interface.tx_linkhandler_tabHandler.php';
 
 /**
  * hook to adjust linkwizard (linkbrowser)
  *
  * @author	Daniel Poetzinger (AOE media GmbH)
+ * @version $Id: $
+ * @date 08.04.2009 - 15:06:25
  * @package TYPO3
- * @subpackage linkhandler
+ * @subpackage tx_linkhandler
+ * @access public
  */
-if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
-
-
-require_once (t3lib_extMgm::extPath('linkhandler').'classes/record/class.tx_linkhandler_recordsTree.php');
-require_once (t3lib_extMgm::extPath('linkhandler').'classes/record/class.TBE_browser_recordListRTE.php');
-require_once (t3lib_extMgm::extPath('linkhandler').'classes/interface.tx_linkhandler_tabHandler.php');
-
-
-
 class tx_linkhandler_recordTab implements tx_linkhandler_tabHandler {
 	protected $isRTE;
 	protected $browseLinksObj;
@@ -213,13 +210,11 @@ class tx_linkhandler_recordTab implements tx_linkhandler_tabHandler {
 
 			// Return accumulated content:
 		return $out;
-
-
-
 	}
-
-
 }
 
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/linkhandler/classes/class.tx_linkhandler_recordTab.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/linkhandler/classes/class.tx_linkhandler_recordTab.php']);
+}
 
 ?>
