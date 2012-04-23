f2629827ae434298aa85cc2c9ac9ef22c2009647
hacksch <hacksch@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-23 08:10:55 +0000

    check disabled field
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/linkhandler/trunk@61000 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_linkhandler_handler.php
+++ class.tx_linkhandler_handler.php
@@ -91,13 +91,17 @@ class tx_linkhandler_handler {
 	 * @author Michael Klapper <michael.klapper@aoemedia.de>
 	 */
 	protected function isRecordLinkable($recordTableName, $linkConfigArray, $recordArray) {
+		global $TCA;
+
 		$isLinkable = false;
 
 			// record type link configuration available
-		if ( is_array($linkConfigArray) && array_key_exists($recordTableName . '.', $linkConfigArray) )  {
+		if ( is_array($recordArray) && !empty($recordArray) ) // recored available
 
 			if (
-					( is_array($recordArray) && !empty($recordArray) ) // recored available
+					( is_array($recordArray) && !empty($recordArray) && !isset( $TCA[$recordTableName]['ctrl']['enablecolumns']['disabled']) ) // recored available
+				|| 
+					( is_array($recordArray) && !empty($recordArray) && isset( $TCA[$recordTableName]['ctrl']['enablecolumns']['disabled'] ) && $recordArray[$TCA[$recordTableName]['ctrl']['enablecolumns']['disabled']] == 0) // record NOT hidden
 				||
 					( (int)$linkConfigArray[$recordTableName . '.']['forceLink'] === 1 ) // if the record are hidden ore someting else, force link generation
 				) {
