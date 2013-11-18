96f7b4155b2f7182704f9dbdefca1d29ef1ad92f
Lacyg <lacyg@cs2.ch>
2013-11-18 13:32:29 +0100

    Update class.tx_linkhandler_handler.php

--- class.tx_linkhandler_handler.php
+++ class.tx_linkhandler_handler.php
@@ -200,9 +200,10 @@ class tx_linkhandler_handler {
 		 * @internal Merge the linkhandler configuration from $linkConfigArray with the current $typoLinkConfiguration.
 		 */
 		if ( is_array($typoLinkConfigurationArray) && !empty($typoLinkConfigurationArray) ) {
-			if ( array_key_exists('parameter.', $typoLinkConfigurationArray) )
+			if ( array_key_exists('parameter.', $typoLinkConfigurationArray) ) {
 				unset($typoLinkConfigurationArray['parameter.']);
-				unset($typoLinkConfigurationArray['parameter']);
+				unset($typoLinkConfigurationArray['parameter']);				
+			}
 
 			$linkConfigArray[$recordTableName . '.'] = array_merge($linkConfigArray[$recordTableName . '.'], $typoLinkConfigurationArray);
 		}
