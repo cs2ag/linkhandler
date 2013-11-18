86a3c98a2c0a657eb86b2298f39ccd45fde7d084
Lacyg <lacyg@cs2.ch>
2013-11-18 15:12:46 +0100

    Update class.tx_linkhandler_handler.php

--- class.tx_linkhandler_handler.php
+++ class.tx_linkhandler_handler.php
@@ -202,8 +202,10 @@ class tx_linkhandler_handler {
 		if ( is_array($typoLinkConfigurationArray) && !empty($typoLinkConfigurationArray) ) {
 			if ( array_key_exists('parameter.', $typoLinkConfigurationArray) ) {
 				unset($typoLinkConfigurationArray['parameter.']);
-				unset($typoLinkConfigurationArray['parameter']);				
 			}
+			if ( array_key_exists('parameter', $typoLinkConfigurationArray) ) {
+				unset($typoLinkConfigurationArray['parameter']);
+			}			
 
 			$linkConfigArray[$recordTableName . '.'] = array_merge($linkConfigArray[$recordTableName . '.'], $typoLinkConfigurationArray);
 		}
